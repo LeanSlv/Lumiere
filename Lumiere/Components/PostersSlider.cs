3deec743327aeaa22bcbfb1bf61f4d132ba04a70
Mihail Solovev <youoliver@mail.ru>
2020-07-18 15:03:21 +0400

    Uploading images to the slider

---
+++ Lumiere/Components/PostersSlider.cs
@@ -0,0 +1,37 @@
+﻿using Lumiere.Models;
+using Lumiere.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lumiere.Components
+{
+    public class PostersSlider : ViewComponent
+    {
+        private readonly ISeanceRepository _seanceRepository;
+
+        public PostersSlider(ISeanceRepository seanceRepository)
+        {
+            _seanceRepository = seanceRepository;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            Dictionary<Guid, string> filmPosters = new Dictionary<Guid, string>();
+
+            List<FilmSeance> seances = _seanceRepository.GetAll().ToList();
+            foreach(FilmSeance seance in seances)
+            {
+                if(seance.Date == DateTime.Today)
+                {
+                    if(seance.Film.Posters.Count >= 2)
+                        filmPosters.Add(seance.Film.Id, seance.Film.Posters[1].Url);
+                }
+            }
+
+            return View("PostersSlider", filmPosters);
+        }
+    }
+}
