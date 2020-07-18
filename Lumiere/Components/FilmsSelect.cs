a68399933fc0960693471a430f0dea3e7bf204fa
Mihail Solovev <youoliver@mail.ru>
2020-07-18 13:32:17 +0400

    Adding a session to movies

---
+++ Lumiere/Components/FilmsSelect.cs
@@ -0,0 +1,37 @@
+﻿using Lumiere.Models;
+using Lumiere.Repositories;
+using Lumiere.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lumiere.Components
+{
+    public class FilmsSelect : ViewComponent
+    {
+        private readonly IFilmRepository _filmRepository;
+
+        public FilmsSelect(IFilmRepository filmRepository)
+        {
+            _filmRepository = filmRepository;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<Film> films = _filmRepository.GetAll().ToList();
+
+            List<FilmsSelectViewModel> filmsSelects = new List<FilmsSelectViewModel>();
+            foreach (Film film in films)
+            {
+                filmsSelects.Add(new FilmsSelectViewModel
+                {
+                    FilmId = film.Id,
+                    FilmName = film.Name
+                });
+            }
+
+            return View("FilmsSelect", filmsSelects);
+        }
+    }
+}
---
+++ Lumiere/ViewModels/FilmsSelectViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Lumiere.ViewModels
+{
+    public class FilmsSelectViewModel
+    {
+        public Guid FilmId { get; set; }
+        public string FilmName { get; set; }
+    }
+}
